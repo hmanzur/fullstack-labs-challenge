da4c264ba29d39f6aabb609ec29dde8f5cc56f69
Habid Enrique Manzur Restrepo <17788438+hmanzur@users.noreply.github.com>
2025-09-24 22:43:43 -0500

    Feature/application deployment (#1)
    * feat(ci): added deployment workflow
    
    * feat(ci): added deployment workflow
    
    * feat(ci): added deployment workflow
    
    * feat(ci): added deployment workflow
    
    * feat(ci): added deployment workflow
    
    * fix(application): prettier fix

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "site" {
-  bucket = "hmanzursoft-fullstack-labs-${var.name}-${var.environment}"
+  bucket = "hmanzursoft-2025-09-${var.name}-${var.environment}"
 }
 
 resource "aws_cloudfront_origin_access_control" "origin" {
