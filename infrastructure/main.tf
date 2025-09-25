b01b79229ad78e5f970f5c76a07552cae61ea412
Habid Enrique Manzur Restrepo <habidmanzur@outlook.com>
2025-09-24 22:26:59 -0500

    feat(ci): added deployment workflow

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "site" {
-  bucket = "hmanzursoft-fullstacklabs-${var.name}-${var.environment}"
+  bucket = "hmanzursoft-fullstack-labs-${var.name}-${var.environment}"
 }
 
 resource "aws_cloudfront_origin_access_control" "origin" {
