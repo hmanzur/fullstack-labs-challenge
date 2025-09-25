50600ec600e6b7e34a87264c521298264d55ddef
Habid Enrique Manzur Restrepo <habidmanzur@outlook.com>
2025-09-24 22:50:06 -0500

    fix(ci): updated different environment set

--- infrastructure/values.tf
+++ infrastructure/values.tf
@@ -1,6 +1,7 @@
 variable "environment" {
   type        = string
   description = "Application Environment select one of devel, stage, prod"
+  default     = "devel"
   validation {
     condition     = contains(["devel", "stage", "prod"], var.environment)
     error_message = "Error: Only can select one of devel, stage or prod"
