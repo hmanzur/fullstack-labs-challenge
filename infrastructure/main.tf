5a18b510f539f2315002f60bf6a87b21e9a7aa7c
Habid Enrique Manzur Restrepo <habidmanzur@outlook.com>
2025-09-24 22:07:53 -0500

    fix(ci): added aws credentials

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "site" {
-  bucket = "${var.name}-${var.environment}"
+  bucket = "hmanzursoft-fullstacklabs-${var.name}-${var.environment}"
 }
 
 resource "aws_cloudfront_origin_access_control" "origin" {
